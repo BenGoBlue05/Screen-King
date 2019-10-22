d6d96cc43ce35c2dc21ae54370775b7aad89618f
Ben Lewis <bplewis5@gmail.com>
2019-10-21 21:39:01 -0500

    Initial Commit

---
+++ Screen King/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Screen King
+//
+//  Created by Benjamin Lewis on 10/21/19.
+//  Copyright © 2019 Benjamin Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
